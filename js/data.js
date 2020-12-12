bb574b235282be8bdea331ec8e17197a49fef47a
Jan Hořava <czechinthemail@gmail.com>
2020-12-12 15:55:59 -0800

    added .mini to intervals input

--- js/data.js
+++ js/data.js
@@ -157,7 +157,7 @@ function new_row(id, amount, insured, interest, interval){
       + '<td><input id="' + row_id + '" value="' + row_id + '">'
       + '<td><input id="' + row_id + '-amount" value="' + amount + '">'
       + '<td><input id="' + row_id + '-interest" value="' + interest + '">'
-      + '<td><input id="' + row_id + '-interval" value="' + interval + '">'
+      + '<td><input class=mini id="' + row_id + '-interval" value="' + interval + '">'
       + '<td><input id="' + row_id + '-insured" value="' + insured + '">'
       + '<td id="' + row_id + '-gain">'
       + '<td id="' + row_id + '-gain-increase">';
