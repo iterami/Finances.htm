7dba7a908bb75131cd5790ffcb3bb870f2e0046f
Jan Hořava <czechinthemail@gmail.com>
2025-06-15 11:21:39 -0700

    simplified property usage

--- repo.js
+++ repo.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function calculate(){
-    core_storage_data['sources'] = JSON.stringify(globalThis.sources);
+    core_storage_data.sources = JSON.stringify(globalThis.sources);
     core_storage_update();
 
     let assets = 0;
@@ -17,16 +17,16 @@ function calculate(){
     };
 
     const sources = globalThis.sources;
-    for(const asset in sources['assets']){
-        const asset_shares = sources['assets'][asset]['shares'];
-        const amount = asset_shares * sources['assets'][asset]['price'];
-        const interval_gain = asset_shares * sources['assets'][asset]['gain'];
+    for(const asset in sources.assets){
+        const asset_shares = sources.assets[asset].shares;
+        const amount = asset_shares * sources.assets[asset].price;
+        const interval_gain = asset_shares * sources.assets[asset].gain;
 
         let gain = 0;
         let gain_increase = 0;
-        gain = interval_gain * sources['assets'][asset]['interval'];
+        gain = interval_gain * sources.assets[asset].interval;
 
-        if(document.getElementById('asset-' + asset + '-apply').checked){
+        if(document.getElementById('asset_' + asset + '_apply').checked){
             assets += 1;
             shares += asset_shares;
             total += amount;
@@ -34,25 +34,25 @@ function calculate(){
             total_increase += gain_increase;
         }
 
-        source_totals['asset-' + asset] = {
+        source_totals['asset_' + asset] = {
           'amount': amount,
           'dividend': interval_gain,
           'gain': gain,
-          'gain-increase': gain_increase,
+          'gain_increase': gain_increase,
         };
     }
 
-    for(const saving in sources['savings']){
-        const amount = sources['savings'][saving]['amount'];
-        const interval_gain = sources['savings'][saving]['gain'];
+    for(const saving in sources.savings){
+        const amount = sources.savings[saving].amount;
+        const interval_gain = sources.savings[saving].gain;
 
         let gain = 0;
         let gain_increase = 0;
-        const interest_percent_year = (interval_gain / 100) * sources['savings'][saving]['interval'];
+        const interest_percent_year = (interval_gain / 100) * sources.savings[saving].interval;
         gain = amount * interest_percent_year;
         gain_increase = (amount + gain) * interest_percent_year - gain;
 
-        if(document.getElementById('savings-' + saving + '-apply').checked){
+        if(document.getElementById('savings_' + saving + '_apply').checked){
             cash += amount;
             savings += 1;
             total += amount;
@@ -60,73 +60,73 @@ function calculate(){
             total_increase += gain_increase;
         }
 
-        source_totals['savings-' + saving] = {
+        source_totals['savings_' + saving] = {
           'amount': amount,
           'gain': gain,
-          'gain-increase': gain_increase,
+          'gain_increase': gain_increase,
         };
     }
 
-    for(const saving in sources['savings']){
-        document.getElementById('savings-' + saving + '-total').innerHTML = format_number(source_totals['savings-' + saving]['gain']);
-        document.getElementById('savings-' + saving + '-total-increase').innerHTML = format_number(source_totals['savings-' + saving]['gain-increase']);
-        document.getElementById('savings-' + saving + '-year-gain-percent').innerHTML = format_number(source_totals['savings-' + saving]['gain'] / total_gain * 100);
+    for(const saving in sources.savings){
+        document.getElementById('savings_' + saving + '_total').innerHTML = format_number(source_totals['savings_' + saving].gain);
+        document.getElementById('savings_' + saving + '_total_increase').innerHTML = format_number(source_totals['savings_' + saving].gain_increase);
+        document.getElementById('savings_' + saving + '_year_gain_percent').innerHTML = format_number(source_totals['savings_' + saving].gain / total_gain * 100);
     }
-    for(const asset in sources['assets']){
-        document.getElementById('asset-' + asset + '-amount').innerHTML = format_number(
-          source_totals['asset-' + asset]['amount'],
+    for(const asset in sources.assets){
+        document.getElementById('asset_' + asset + '_amount').innerHTML = format_number(
+          source_totals['asset_' + asset].amount,
           2
         );
-        document.getElementById('asset-' + asset + '-dividend').innerHTML = format_number(source_totals['asset-' + asset]['dividend']);
-        document.getElementById('asset-' + asset + '-total').innerHTML = format_number(source_totals['asset-' + asset]['gain']);
-        document.getElementById('asset-' + asset + '-gain-percent').innerHTML = format_number(source_totals['asset-' + asset]['gain'] / source_totals['asset-' + asset]['amount'] * 100);
-        document.getElementById('asset-' + asset + '-year-gain-percent').innerHTML = format_number(source_totals['asset-' + asset]['gain'] / total_gain * 100);
+        document.getElementById('asset_' + asset + '_dividend').innerHTML = format_number(source_totals['asset_' + asset].dividend);
+        document.getElementById('asset_' + asset + '_total').innerHTML = format_number(source_totals['asset_' + asset].gain);
+        document.getElementById('asset_' + asset + '_gain_percent').innerHTML = format_number(source_totals['asset_' + asset].gain / source_totals['asset_' + asset].amount * 100);
+        document.getElementById('asset_' + asset + '_year_gain_percent').innerHTML = format_number(source_totals['asset_' + asset].gain / total_gain * 100);
     }
 
-    core_elements['assets'].innerHTML = format_number(
+    core_elements.assets.innerHTML = format_number(
       assets,
       0
     );
-    core_elements['cash'].innerHTML = format_number(
+    core_elements.cash.innerHTML = format_number(
       cash,
       2
     );
-    core_elements['cash-percent'].innerHTML = format_number((cash / total) * 100);
-    core_elements['savings'].innerHTML = format_number(
+    core_elements.cash_percent.innerHTML = format_number((cash / total) * 100);
+    core_elements.savings.innerHTML = format_number(
       savings,
       0
     );
-    core_elements['shares'].innerHTML = format_number(
+    core_elements.shares.innerHTML = format_number(
       shares,
       0
     );
-    core_elements['total'].innerHTML = format_number(
+    core_elements.total.innerHTML = format_number(
       total,
       2
     );
-    core_elements['value'].innerHTML = format_number(
+    core_elements.value.innerHTML = format_number(
       total - cash,
       2
     );
-    core_elements['value-percent'].innerHTML = format_number(((total - cash) / total) * 100);
+    core_elements.value_percent.innerHTML = format_number(((total - cash) / total) * 100);
 
-    for(const asset in sources['assets']){
-        document.getElementById('asset-' + asset + '-percent').innerHTML = format_number(source_totals['asset-' + asset]['amount'] / total * 100);
+    for(const asset in sources.assets){
+        document.getElementById('asset_' + asset + '_percent').innerHTML = format_number(source_totals['asset_' + asset].amount / total * 100);
     }
-    for(const saving in sources['savings']){
-        document.getElementById('savings-' + saving + '-percent').innerHTML = format_number(source_totals['savings-' + saving]['amount'] / total * 100);
+    for(const saving in sources.savings){
+        document.getElementById('savings_' + saving + '_percent').innerHTML = format_number(source_totals['savings_' + saving].amount / total * 100);
     }
 
     for(const interval in intervals){
         const increase = total_gain / intervals[interval];
         const increase_year = total_increase / intervals[interval];
 
-        core_elements['total-' + interval].innerHTML = format_number(increase);
-        core_elements['total-' + interval + '-percent'].innerHTML = total === 0
+        core_elements['total_' + interval].innerHTML = format_number(increase);
+        core_elements['total_' + interval + '_percent'].innerHTML = total === 0
           ? ''
           : format_number((increase / total) * 100);
-        core_elements['total-' + interval + '-increase-' + interval].innerHTML = format_number(increase_year / intervals[interval]);
-        core_elements['total-' + interval + '-increase-yearly'].innerHTML = format_number(increase_year);
+        core_elements['total_' + interval + '_increase_' + interval].innerHTML = format_number(increase_year / intervals[interval]);
+        core_elements['total_' + interval + '_increase_yearly'].innerHTML = format_number(increase_year);
     }
 
     calculate_info();
@@ -134,7 +134,7 @@ function calculate(){
 
 function calculate_info(){
     core_storage_save([
-      'goal-time',
+      'goal_time',
       'tax',
     ]);
 
@@ -142,12 +142,12 @@ function calculate_info(){
       'patterns': {
         ',': '',
       },
-      'string': core_elements['total-hour'].textContent,
+      'string': core_elements.total_hour.textContent,
     })) / 3600;
-    core_elements['goal-time-gain'].innerHTML = gain_per_second <= 0
+    core_elements.goal_time_gain.innerHTML = gain_per_second <= 0
       ? ''
       : time_format({
-          'date': timestamp_to_date(core_storage_data['goal-time'] / gain_per_second * 1000),
+          'date': timestamp_to_date(core_storage_data.goal_time / gain_per_second * 1000),
           'diff': true,
           'milliseconds': true,
         });
@@ -156,9 +156,9 @@ function calculate_info(){
       'patterns': {
         ',': '',
       },
-      'string': core_elements['total-year'].textContent,
+      'string': core_elements.total_year.textContent,
     }));
-    core_elements['tax-result'].innerHTML = format_number(gain_per_year * (core_elements['tax'].value / 100));
+    core_elements.tax_result.innerHTML = format_number(gain_per_year * (core_elements.tax.value / 100));
 }
 
 function format_number(number, pad){
@@ -196,7 +196,7 @@ function format_number(number, pad){
 }
 
 function new_asset(id, shares, price, gain, interval){
-    sources['assets'][id] = {
+    sources.assets[id] = {
       'shares': shares,
       'price': price,
       'gain': gain,
@@ -204,62 +204,62 @@ function new_asset(id, shares, price, gain, interval){
     };
 
     return '<tr>'
-      + '<td><input id="asset-' + id + '-apply" onclick="calculate()" type=checkbox checked>'
+      + '<td><input id="asset_' + id + '_apply" onclick="calculate()" type=checkbox checked>'
       + '<td>' + id
       + '<td>' + format_number(shares, 0)
       + '<td>' + format_number(price, 2)
-      + '<td id="asset-' + id + '-amount">'
-      + '<td id="asset-' + id + '-percent">'
+      + '<td id="asset_' + id + '_amount">'
+      + '<td id="asset_' + id + '_percent">'
       + '<td>' + format_number(gain)
       + '<td class=center>' + interval
-      + '<td id="asset-' + id + '-dividend">'
-      + '<td id="asset-' + id + '-total">'
-      + '<td id="asset-' + id + '-gain-percent">'
-      + '<td id="asset-' + id + '-year-gain-percent">';
+      + '<td id="asset_' + id + '_dividend">'
+      + '<td id="asset_' + id + '_total">'
+      + '<td id="asset_' + id + '_gain_percent">'
+      + '<td id="asset_' + id + '_year_gain_percent">';
 }
 
 function new_savings(id, amount, gain, interval){
-    sources['savings'][id] = {
+    sources.savings[id] = {
       'amount': amount,
       'gain': gain,
       'interval': interval,
     };
 
     return '<tr>'
-      + '<td><input id="savings-' + id + '-apply" onclick="calculate()" type=checkbox checked>'
+      + '<td><input id="savings_' + id + '_apply" onclick="calculate()" type=checkbox checked>'
       + '<td>' + id
       + '<td>' + format_number(amount, 2)
-      + '<td id="savings-' + id + '-percent">'
+      + '<td id="savings_' + id + '_percent">'
       + '<td>' + gain
-      + '<td id="savings-' + id + '-total">'
-      + '<td id="savings-' + id + '-total-increase">'
-      + '<td id="savings-' + id + '-year-gain-percent">';
+      + '<td id="savings_' + id + '_total">'
+      + '<td id="savings_' + id + '_total_increase">'
+      + '<td id="savings_' + id + '_year_gain_percent">';
 }
 
 function repo_init(){
     core_repo_init({
       'events': {
-        'assets-all': {
+        'assets_all': {
           'onchange': function(){
-              const assets = globalThis.sources['assets'];
+              const assets = globalThis.sources.assets;
               for(const asset in assets){
-                  document.getElementById('asset-' + asset + '-apply').checked = this.checked;
+                  document.getElementById('asset_' + asset + '_apply').checked = this.checked;
               }
 
               calculate();
           },
         },
-        'savings-all': {
+        'savings_all': {
           'onchange': function(){
-              const savings = globalThis.sources['savings'];
+              const savings = globalThis.sources.savings;
               for(const saving in savings){
-                  document.getElementById('savings-' + saving + '-apply').checked = this.checked;
+                  document.getElementById('savings_' + saving + '_apply').checked = this.checked;
               }
 
               calculate();
           },
         },
-        'goal-time': {
+        'goal_time': {
           'oninput': calculate_info,
         },
         'tax': {
@@ -284,7 +284,7 @@ function repo_init(){
         },
       },
       'storage': {
-        'goal-time': 1,
+        'goal_time': 1,
         'sources': '{"assets":{"example asset":{"shares":100,"price":25,"gain":1,"interval":12}},"savings":{"example savings":{"amount":5000,"gain":0.23,"interval":12}}}',
         'tax': 15,
       },
@@ -293,15 +293,15 @@ function repo_init(){
       'ui-elements': [
         'assets',
         'cash',
-        'cash-percent',
-        'goal-time-gain',
+        'cash_percent',
+        'goal_time_gain',
         'savings',
         'shares',
         'tax',
-        'tax-result',
+        'tax_result',
         'total',
         'value',
-        'value-percent',
+        'value_percent',
       ],
     });
 
@@ -310,56 +310,56 @@ function repo_init(){
         intervalsbody += '<tr>'
           + '<td>' + interval
           + '<td>' + (interval === 'custom'
-            ? '<input class=mini id=total-custom-intervals step=any type=number value=.5>'
+            ? '<input class=mini id=total_custom_intervals step=any type=number value=.5>'
             : intervals[interval])
-          + '<td id=total-' + interval + '>'
-          + '<td id=total-' + interval + '-percent>'
-          + '<td id=total-' + interval + '-increase-' + interval + '>'
-          + '<td id=total-' + interval + '-increase-yearly>';
+          + '<td id=total_' + interval + '>'
+          + '<td id=total_' + interval + '_percent>'
+          + '<td id=total_' + interval + '_increase_' + interval + '>'
+          + '<td id=total_' + interval + '_increase_yearly>';
     }
-    document.getElementById('intervals-body').innerHTML = intervalsbody;
-    document.getElementById('total-custom-intervals').oninput = function(){
+    document.getElementById('intervals_body').innerHTML = intervalsbody;
+    document.getElementById('total_custom_intervals').oninput = function(){
         let customintervals = this.value;
         if(globalThis.isNaN(customintervals)
           || customintervals < 0){
             customintervals = 0;
         }
-        intervals['custom'] = customintervals;
+        intervals.custom = customintervals;
         calculate();
     };
     for(const interval in intervals){
-        core_elements['total-' + interval] = document.getElementById('total-' + interval);
-        core_elements['total-' + interval + '-percent'] = document.getElementById('total-' + interval + '-percent');
-        core_elements['total-' + interval + '-increase-' + interval] = document.getElementById('total-' + interval + '-increase-' + interval);
-        core_elements['total-' + interval + '-increase-yearly'] = document.getElementById('total-' + interval + '-increase-yearly');
+        core_elements['total_' + interval] = document.getElementById('total_' + interval);
+        core_elements['total_' + interval + '_percent'] = document.getElementById('total_' + interval + '_percent');
+        core_elements['total_' + interval + '_increase_' + interval] = document.getElementById('total_' + interval + '_increase_' + interval);
+        core_elements['total_' + interval + '_increase_yearly'] = document.getElementById('total_' + interval + '_increase_yearly');
     }
 
-    const json = JSON.parse(core_storage_data['sources']);
+    const json = JSON.parse(core_storage_data.sources);
 
     let rows = '';
-    const assets = json['assets'];
+    const assets = json.assets;
     for(const asset in assets){
         rows += new_asset(
           asset,
-          assets[asset]['shares'],
-          assets[asset]['price'],
-          assets[asset]['gain'],
-          assets[asset]['interval']
+          assets[asset].shares,
+          assets[asset].price,
+          assets[asset].gain,
+          assets[asset].interval
         );
     }
-    document.getElementById('assets-body').innerHTML = rows;
+    document.getElementById('assets_body').innerHTML = rows;
 
     rows = '';
-    const savings = json['savings'];
+    const savings = json.savings;
     for(const saving in savings){
         rows += new_savings(
           saving,
-          savings[saving]['amount'],
-          savings[saving]['gain'],
-          savings[saving]['interval']
+          savings[saving].amount,
+          savings[saving].gain,
+          savings[saving].interval
         );
     }
-    document.getElementById('savings-body').innerHTML = rows;
+    document.getElementById('savings_body').innerHTML = rows;
 
     calculate();
 }
